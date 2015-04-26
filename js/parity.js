d0526c69f17c239719b7afbc4a3c0add86a34499
cesar <cesar@romero.ws>
2015-04-26 00:21:24 -0700

    render x and y axis

--- js/parity.js
+++ js/parity.js
@@ -2,7 +2,7 @@ angular.module('parity', [])
     .controller('ParityCtrl', function($log, $scope) {
 
         // global attrs
-        var width, height, svg;
+        var margin, width, height, svg;
 
         // draws the X axis using D3
         function xAxis() {
@@ -11,12 +11,16 @@ angular.module('parity', [])
                 .domain([0,9])
                 .range([0,width]);
             var axis = d3.svg.axis()
-                .scale(scale)
-                .ticks(10);
-            svg.call(axis);
+                    .orient('bottom')
+                    .scale(scale)
+                    .ticks(10);
+            svg.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', 'translate(0,'+height+')')
+                .call(axis);
         }
 
-        // drwas nothing for now.
+        // draws nothing for now.
         function yAxis() {
             $log.debug('y axis');
             var scale = d3.scale.linear()
@@ -26,7 +30,9 @@ angular.module('parity', [])
                 .orient('left')
                 .scale(scale)
                 .ticks(10);
-            svg.call(axis);
+            svg.append('g')
+                .attr('class', 'y axis')
+                .call(axis);
         }
 
         // draws the grid where nodes and edges will be placed.
@@ -40,18 +46,25 @@ angular.module('parity', [])
         // initialized.
         function init() {
             $log.debug('init');
-            height = 500;
-            width = 600;
+            margin = {top: 20, right: 20, bottom: 30, left: 40};
+            height = 500 - margin.top - margin.bottom;
+            width = 960 - margin.left - margin.right;
             svg = d3.select("#game")
                 .append("svg")
                 .attr("class", "axis")
-                .attr("width", width)
-                .attr("height", height)
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
                 .append("g")
-                .attr("transform", "translate(0,30)");
+                .attr("transform", 'translate(' + margin.left + ',' + margin.top + ')');
+            // draw the grid
             grid();
-        }
 
+            // Let's try to draw a circle on click
+            d3.select('#game').on('click', function(d, i) {
+                $log.debug('clicked d,i=' + d +','+i);
+                $log.debug('mouse:' + angular.toJson(d3.mouse(svg)));
+            });
+        }
         // set things up.
-        init()
+        init();
     });
