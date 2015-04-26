068c9e64125acda80d2c0196a759c40b2bd897c6
cesar <cesar@romero.ws>
2015-04-26 01:23:19 -0700

    Adds primitive mode switching

---
+++ js/parity-edit.js
@@ -0,0 +1,70 @@
+angular.module('parity')
+    .controller('ParityEditCtrl', function($log, $scope) {
+
+        // global attrs
+        var margin, width, height, svg;
+
+        // draws the X axis using D3
+        function xAxis() {
+            $log.debug('x axis');
+            var scale = d3.scale.linear()
+                .domain([0,9])
+                .range([0,width]);
+            var axis = d3.svg.axis()
+                    .orient('bottom')
+                    .scale(scale)
+                    .ticks(10);
+            svg.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', 'translate(0,'+height+')')
+                .call(axis);
+        }
+
+        // draws nothing for now.
+        function yAxis() {
+            $log.debug('y axis');
+            var scale = d3.scale.linear()
+                .domain([0,9])
+                .range([0, height]);
+            var axis = d3.svg.axis()
+                .orient('left')
+                .scale(scale)
+                .ticks(10);
+            svg.append('g')
+                .attr('class', 'y axis')
+                .call(axis);
+        }
+
+        // draws the grid where nodes and edges will be placed.
+        function grid() {
+            $log.debug('grid');
+            xAxis();
+            yAxis();
+        }
+
+        // initializes the svg elem and anything else that needs to be
+        // initialized.
+        function init() {
+            $log.debug('init');
+            margin = {top: 20, right: 20, bottom: 30, left: 40};
+            height = 500 - margin.top - margin.bottom;
+            width = 960 - margin.left - margin.right;
+            svg = d3.select("#editor")
+                .append("svg")
+                .attr("class", "axis")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", 'translate(' + margin.left + ',' + margin.top + ')');
+            // draw the grid
+            grid();
+
+            // Let's try to draw a circle on click
+            d3.select('#game').on('click', function(d, i) {
+                $log.debug('clicked d,i=' + d +','+i);
+                $log.debug('mouse:' + angular.toJson(d3.mouse(svg)));
+            });
+        }
+        // set things up.
+        init();
+    });
--- js/parity.js
+++ js/parity.js
@@ -1,70 +1,22 @@
 angular.module('parity', [])
-    .controller('ParityCtrl', function($log, $scope) {
+    .controller('ParityNavCtrl', function($log, $scope) {
+        // This simple controller just takes care of navigation and
+        // switching between modes. We may use ngroute if necessary in
+        // the future, but that seems like overkill for now.
+        $scope.modes = [
+            {name: 'Edit', template: 'edit.html'},
+            {name: 'Play', template: 'play.html'},
+        ];
+        var _mode = $scope.modes[0];
 
-        // global attrs
-        var margin, width, height, svg;
-
-        // draws the X axis using D3
-        function xAxis() {
-            $log.debug('x axis');
-            var scale = d3.scale.linear()
-                .domain([0,9])
-                .range([0,width]);
-            var axis = d3.svg.axis()
-                    .orient('bottom')
-                    .scale(scale)
-                    .ticks(10);
-            svg.append('g')
-                .attr('class', 'x axis')
-                .attr('transform', 'translate(0,'+height+')')
-                .call(axis);
-        }
-
-        // draws nothing for now.
-        function yAxis() {
-            $log.debug('y axis');
-            var scale = d3.scale.linear()
-                .domain([0,9])
-                .range([0, height]);
-            var axis = d3.svg.axis()
-                .orient('left')
-                .scale(scale)
-                .ticks(10);
-            svg.append('g')
-                .attr('class', 'y axis')
-                .call(axis);
-        }
-
-        // draws the grid where nodes and edges will be placed.
-        function grid() {
-            $log.debug('grid');
-            xAxis();
-            yAxis();
-        }
-
-        // initializes the svg elem and anything else that needs to be
-        // initialized.
-        function init() {
-            $log.debug('init');
-            margin = {top: 20, right: 20, bottom: 30, left: 40};
-            height = 500 - margin.top - margin.bottom;
-            width = 960 - margin.left - margin.right;
-            svg = d3.select("#game")
-                .append("svg")
-                .attr("class", "axis")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", 'translate(' + margin.left + ',' + margin.top + ')');
-            // draw the grid
-            grid();
-
-            // Let's try to draw a circle on click
-            d3.select('#game').on('click', function(d, i) {
-                $log.debug('clicked d,i=' + d +','+i);
-                $log.debug('mouse:' + angular.toJson(d3.mouse(svg)));
-            });
-        }
-        // set things up.
-        init();
+        // This behavior updates the active mode and saves any state
+        // necessary.
+        $scope.mode = function(newmode) {
+            $log.debug('mode with params:' + angular.toJson(newmode));
+            if(newmode) {
+                _mode = newmode;
+            } else {
+                return _mode;
+            }
+        };
     });
